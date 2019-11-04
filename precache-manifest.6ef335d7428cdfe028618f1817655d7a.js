112608ca242267b650a8a393866b744be39ee454
viskoos <potniye@gmail.com>
2019-11-04 13:49:46 +0300

    Updates

---
+++ precache-manifest.6ef335d7428cdfe028618f1817655d7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b16068240101de7f5e01e067a3c0d4df",
+    "url": "/justalehas/index.html"
+  },
+  {
+    "revision": "92d2d7af335ac4f960da",
+    "url": "/justalehas/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "fe4b3b2cd3fa28c9c038",
+    "url": "/justalehas/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "92d2d7af335ac4f960da",
+    "url": "/justalehas/static/js/2.a028914f.chunk.js"
+  },
+  {
+    "revision": "fe4b3b2cd3fa28c9c038",
+    "url": "/justalehas/static/js/main.787a06dc.chunk.js"
+  },
+  {
+    "revision": "c42022efe15ef7283609",
+    "url": "/justalehas/static/js/runtime-main.58aa7323.js"
+  }
+]);
